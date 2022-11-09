d5796447fd906c7c46a646e20ae74a6e66a49567
SHozab <53463245+SHozab29@users.noreply.github.com>
2022-11-09 22:00:57 +0530

    nov 9

--- project/buyPage/views.py
+++ project/buyPage/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+from index import views
 
 def home(request):
-    return render(request, 'buyPage.html')
+    return render(request, 'buyPage.html',{'price' : 700})
+
+def contact(request):
+    return redirect('/#contact')
